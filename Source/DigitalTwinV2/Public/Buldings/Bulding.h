6f200ae45c0d41b851075f8dd597a89f35a4f0e1
ON4Saleh <116005799+ON4Saleh@users.noreply.github.com>
2025-06-12 03:13:34 +0300

    set card

--- Source/DigitalTwinV2/Public/Buldings/Bulding.h
+++ Source/DigitalTwinV2/Public/Buldings/Bulding.h
@@ -16,61 +16,67 @@ enum class EBuildingStatus : uint8
 	UnderConstruction UMETA(DisplayName = "Under Construction")
 };
 
+UENUM(BlueprintType)
+enum class EBuildingType : uint8
+{
+    OneStoryVilla     UMETA(DisplayName = "One Story Villa"),
+    Palace            UMETA(DisplayName = "Palace"),
+    StandAloneVilla   UMETA(DisplayName = "Stand Alone Villa")
+};
+
 UCLASS()
 class DIGITALTWINV2_API ABulding : public AActor
 {
 	GENERATED_BODY()
-
 public:
-	// Sets default values for this actor's properties
 	ABulding();
-
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
 public:
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-
-
-
-	//data
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Budling")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Building")
 	int32 Id;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Budling")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Building")
 	int32 NumberOfRooms;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Budling")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Building")
 	float FootPrintArea;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Budling")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Building")
 	float BuldingUnitArea;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Budling")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Building")
+	float GroundFloorArea;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Building")
+	float FirstFloorArea;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Building")
+	float RoofFloorArea;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Building")
 	float Price;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Budling")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Building")
 	float Discount;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Budling")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Building")
 	EBuildingStatus Status;
-
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Building")
+	EBuildingType BuildingType;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Building")
 	FString ImagePath;
-
-
-
-
-
-
-	//components
+	UFUNCTION(BlueprintCallable, Category = "Building")
+	float GetFinalPrice() const
+	{
+		return Price - (Price * Discount / 100.0f);
+	}
+	UFUNCTION(BlueprintCallable, Category = "Building")
+	FString GetStatusAsString() const
+	{
+		switch (Status)
+		{
+			case EBuildingStatus::Available: return TEXT("Available");
+			case EBuildingStatus::Reserved: return TEXT("Reserved");
+			case EBuildingStatus::Sold: return TEXT("Sold");
+			case EBuildingStatus::UnderConstruction: return TEXT("Under Construction");
+			default: return TEXT("Unknown");
+		}
+	}
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	class UStaticMeshComponent* Mesh;
-
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	class UBoxComponent* BoxCollider;
-
 };
