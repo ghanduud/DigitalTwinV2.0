126fecc5577b526dfa5920c93347e1d62ed8ff43
ON4Saleh <116005799+ON4Saleh@users.noreply.github.com>
2025-06-18 12:38:34 +0300

    fix ui

--- Source/DigitalTwinV2/Public/UI/FilterAvailability.h
+++ Source/DigitalTwinV2/Public/UI/FilterAvailability.h
@@ -1,17 +1,17 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// // Fill out your copyright notice in the Description page of Project Settings.
 
-#pragma once
+// #pragma once
 
-#include "CoreMinimal.h"
-#include "Blueprint/UserWidget.h"
-#include "FilterAvailability.generated.h"
+// #include "CoreMinimal.h"
+// #include "Blueprint/UserWidget.h"
+// #include "FilterAvailability.generated.h"
 
-/**
- * 
- */
-UCLASS()
-class UFilterAvailability : public UUserWidget
-{
-	GENERATED_BODY()
+// /**
+//  * 
+//  */
+// UCLASS()
+// class UFilterAvailability : public UUserWidget
+// {
+// 	GENERATED_BODY()
 	
-};
+// };
